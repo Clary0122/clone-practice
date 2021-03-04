17696a63fd867ab1ff1dba06f92c60f32b9bfb1d
chaeyun0122 <chaeyun97@gmail.com>
2021-03-04 11:10:57 +0900

    refactor: Set All Srtring to Uppercase

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,10 @@
 for i in range(1, 101):
     if i % 15 == 0:
-        print('fizzbuzz')
+        print('FIZZBUZZ')
     elif i % 3 == 0:
-        print('fizz')
+        print('FIZZ')
     elif i % 5 == 0:
-        print('buzz')
+        print('BUZZ')
     else:
         print(i)
 
