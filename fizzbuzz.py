cdf8ca4e5713a941f9c7305de18215207d5aca60
chaeyun0122 <chaeyun97@gmail.com>
2021-03-04 11:39:30 +0900

    refactor: Change to list comprehension

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,1 @@
-for i in range(1, 101):
-    if i % 15 == 0:
-        print('피즈버즈')
-    elif i % 3 == 0:
-        print('FIZZ')
-    elif i % 5 == 0:
-        print('BUZZ')
-    else:
-        print(i)
+print(['fizzbuzz' if i%15==0 else 'fizz' if i%3==0 else 'buzz' if i%5==0 else i for i in range(1,101)])
