9efc1fa5dc6b377430b09c5125e8be3ac10f569f
chaeyun0122 <chaeyun97@gmail.com>
2021-03-04 14:28:47 +0900

    refactor: tial 3

--- numguess/game.py
+++ numguess/game.py
@@ -8,9 +8,16 @@ print(answer)
 
 guess = int(input('welcome {}. Guess the number: '.format(user_name)))
 
-print(answer, guess)
+n=1
 
-if guess==answer:
-    print('Correct!')
-else:
-    print('Wrong!! The answer was {}'.format(answer))
+while (n!=3):
+    if guess != answer:
+        n += 1
+        print('wrong!! Try again')
+        guess = int(input('Guess the number: '))
+        if n==3:
+            print('Wrong!! The answer was {}'.format(answer))
+    else:
+        print('Correct!')
+        break
+
