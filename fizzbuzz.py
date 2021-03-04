c588d86e695d6af8bd53a53643e310479d64cdae
chaeyun0122 <chaeyun97@gmail.com>
2021-03-04 11:08:16 +0900

    refactor: Change All String to Korean

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,10 @@
 for i in range(1, 101):
     if i % 15 == 0:
-        print('fizzbuzz')
+        print('피즈버즈')
     elif i % 3 == 0:
-        print('fizz')
+        print('피즈')
     elif i % 5 == 0:
-        print('buzz')
+        print('버즈')
     else:
         print(i)
 
