8d69b98e46cc96e953d4b9e6fb5c09dd855396d0
chaeyun0122 <chaeyun97@gmail.com>
2021-03-04 10:50:09 +0900

    refector: Modify simple

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,10 @@
 for i in range(1, 101):
-    if i % 3 == 0 and i % 15 != 0:
-        print('fizz')
-    elif i % 5 == 0 and i % 15 != 0:
-        print('buzz')
-    elif i % 15 == 0:
+    if i % 15 == 0:
         print('fizzbuzz')
+    elif i % 3 == 0:
+        print('fizz')
+    elif i % 5 == 0:
+        print('buzz')
     else:
         print(i)
 
